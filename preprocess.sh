df81e09e809b724884a52d91cc91b1e708d4746a
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2020-05-07 02:34:09 +0200

    ini and scripúts

---
+++ preprocess.sh
@@ -0,0 +1,7 @@
+neuralmonkey/scripts/imagenet_features.py \
+    --input-prefix data/test \
+    --output-prefix data/test_preprocessed/ \
+    --images images.txt \
+    --model-checkpoint models/resnet/ \
+    --slim-models models/tfmodels/research/slim/
+
